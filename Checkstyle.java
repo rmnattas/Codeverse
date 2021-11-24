8cc597a3b83216598df127f788d2040630eca0ee
Abdulrahman Alattas <rmnattas@gmail.com>
2021-11-24 13:07:19 -0700

    Executer update

--- Checkstyle.java
+++ Checkstyle.java
@@ -3,13 +3,14 @@ public class Checkstyle {
     final static String GOOGLE_STANDARD_PATH = "./google_checks.xml";
     final static String SUN_STANDARD_PATH = "./sun_checks.xml";
 
+    // gets the .java files
     public static void run(String codePath){
         run(codePath, GOOGLE_STANDARD_PATH);
     }
 
     public static void run(String codePath, String standardPath){
-        // String[] command = {"java", "-jar", "./checkstyle-9.1-all.jar", "-c", standardPath, codePath};
-        String[] command = {"pwd"};
-        Executer.exec(command);
+        String[] command = {"java", "-jar", "./checkstyle-9.1-all.jar", "-c", standardPath, codePath};
+        Process p = Executer.exec(command);
+        Executer.printResults(p);
     }
 }
--- Executer.java
+++ Executer.java
@@ -2,28 +2,34 @@ import java.lang.Runtime;
 import java.io.*;
 
 public class Executer {
-    public static void exec(String[] command){
+    public static Process exec(String[] command){
+        Process process = null;
         try{
-            Process process = Runtime.getRuntime().exec(command);
-            printResults(process);  // FOR TESTING
+            process = Runtime.getRuntime().exec(command);
         }catch (Exception e){
             System.out.println(e);
         }
+        return process;
     }
-    public static void exec(String command){
+    public static Process exec(String command){
+        Process process = null;
         try{
-            Process process = Runtime.getRuntime().exec(command);
-            printResults(process);  // FOR TESTING
+            process = Runtime.getRuntime().exec(command);
         }catch (Exception e){
             System.out.println(e);
         }
+        return process;
     }
 
-    public static void printResults(Process process) throws IOException {
+    public static void printResults(Process process){
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line = "";
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+        try{
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e){
+            System.out.println(e);
         }
     }
 }
