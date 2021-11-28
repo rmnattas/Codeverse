02f73950a3fec1b4ccb32487005998f5d3b470c3
Abdulrahman Alattas <rmnattas@gmail.com>
2021-11-27 19:29:16 -0700

    spotbugs check num 2

--- SpotBugs.java
+++ SpotBugs.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class SpotBugs {
 
     /*
     Checks implemented (from http://findbugs.sourceforge.net/bugDescriptions.html)
     - M C UR: Uninitialized read
+    - M P UuF: Unused field
     - 
     */
+
+    final static String[] CHECKS = {"M C UR", "M P UuF"};
+
 
     public static void run(String classPath){
         String[] command = {"java", "-jar", "lib/spotbugs/spotbugs.jar", "-textui", "test/"};
@@ -18,40 +24,56 @@ public class SpotBugs {
         }
     }
 
-// can be improved with 2D array
-public static ArrayList<String> formatAll(ArrayList<String> output){
-    // init ArrayList
-    ArrayList<String> formattedOutput = new ArrayList<String>();
-    ArrayList<String> rawMCUR = new ArrayList<String>();
+    public static ArrayList<String> formatAll(ArrayList<String> output){
+        // init
+        HashMap<String,HashSet<String>> outputMap = new HashMap<String,HashSet<String>>();
+        ArrayList<String> formattedOutput = new ArrayList<String>();
+        for (int i=0; i<CHECKS.length; i++)
+            outputMap.put(CHECKS[i], new HashSet<String>()); 
 
-    // split output
-    for (String s : output){
-        if (s.contains("M C UR")){
-            rawMCUR.add(s);
+        // split output to its checks sets
+        for (String s : output){
+            for (int i=0; i<CHECKS.length; i++){
+                if (s.contains(CHECKS[i])){
+                    outputMap.get(CHECKS[i]).add(s);
+                    break;
+                }
+            }
         }
+
+        // pass each check set to its formatter
+        formattedOutput.addAll(formatMCUR(outputMap.get("M C UR")));
+        formattedOutput.addAll(formatMPUuF(outputMap.get("M P UuF")));
+
+        return formattedOutput;
+        // return output;
     }
-    
-    // pass each output to its formatter
-    ArrayList<String> formattedMCUR = formatMCUR(rawMCUR);
 
-    // merge formatted outputs
-    formattedOutput.addAll(formattedMCUR);
+    public static ArrayList<String> formatMCUR(HashSet<String> arr){
+        ArrayList<String> out = new ArrayList<String>();
 
-    return formattedOutput;
-    // return output;
-}
+        if (arr.size() > 0)
+            out.add("Variables should not be read before initialization, there is " + arr.size() + " accesses to uninitialized variables:");
 
-public static ArrayList<String> formatMCUR(ArrayList<String> arr){
-    ArrayList<String> out = new ArrayList<String>();
-    String[] vars = new String[arr.size()];
-    int i = 0;
-    for (String s : arr){
-        String[] tokens = s.split(" ");
-        vars[i] = tokens[6];
-        i += 1;
+        for (String s : arr){
+            String[] tokens = s.split(" ");
+            out.add(" - " + tokens[tokens.length-2] + tokens[tokens.length-1] + " \'" + tokens[6] + "\' in " + tokens[9]);
+        }
+
+        return out;
     }
-    if (vars.length > 0)
-        out.add("Variables should not be read before initialization, there is " + arr.size() + " accessed uninitialized variables: " + String.join(", ", vars) + ".");
-    return out;
-}
+
+    public static ArrayList<String> formatMPUuF(HashSet<String> arr){
+        ArrayList<String> out = new ArrayList<String>();
+
+        if (arr.size() > 0)
+            out.add("There is " + arr.size() + " fields that are never used, consider removing them from the code:");
+
+        for (String s : arr){
+            String[] tokens = s.split(" ");
+            out.add(" - " + tokens[5] + " in " + tokens[tokens.length-1]);
+        }
+
+        return out;
+    }
 }
